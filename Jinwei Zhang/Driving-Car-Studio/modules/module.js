b9782af805cef62d7cf475c0106e709eb7e532c1
jinwei14 <janviezhang14@gmail.com>
2019-04-25 15:01:53 +0100

    manage to move the car to the traffic light

--- Jinwei Zhang/Driving-Car-Studio/modules/module.js
+++ Jinwei Zhang/Driving-Car-Studio/modules/module.js
@@ -1,6 +1,6 @@
 module.exports = (engine, program) => {
   engine.define('testPrint', (message) => {
-    console.log('testPrint: ' + message.evaluate());
+    console.log( message.evaluate());
     return [{ theta: {} }];
   });
 };
