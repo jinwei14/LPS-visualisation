bce13e250d03d93eb061f22cc3f4a5afb89569ac
jinwei14 <janviezhang14@gmail.com>
2019-05-18 10:52:56 +0100

    learning curve

--- Jinwei Zhang/NodejsLearning/index.js
+++ Jinwei Zhang/NodejsLearning/index.js
@@ -4,3 +4,12 @@ const Person = require('./person'); // common js
 const person1 = new Person('John Doe', 30);
 
 person1.greeting();
+
+
+const LPS = require('lps');
+LPS.loadFile('emoji.lps')
+  .then((engine) => {
+      engine.run();
+  });
+
+//old-style callbacks and newer promise-style code
