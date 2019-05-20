39cba5cd81d89d6fa094f2a033254be3285229af
jinwei14 <janviezhang14@gmail.com>
2019-05-20 13:06:46 +0100

    updating to example

--- Jinwei_Zhang/lps.js/src/parser/AstNode.js
+++ Jinwei_Zhang/lps.js/src/parser/AstNode.js
@@ -2,15 +2,15 @@
   This file is part of the lps.js project, released open source under
   the BSD 3-Clause license. For more info, please see https://github.com/mauris/lps.js
  */
- const lpsRequire = require('../lpsRequire');
+const lpsRequire = require('../lpsRequire');
+// eslint-disable-next-line no-unused-vars
 const NodeTypes = lpsRequire('parser/NodeTypes');
 
 function AstNode(type, token) {
   // if (type == NodeTypes.Program){
   //   console.log(type);
   //   console.log(token);
-  // }
-  
+
 
   let _type = type;
   let _token = token;
