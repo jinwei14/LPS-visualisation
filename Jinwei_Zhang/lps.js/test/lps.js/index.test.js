72433e2019441cf8833acda53938a46de9e81a97
jinwei14 <janviezhang14@gmail.com>
2019-05-24 11:01:29 +0100

    find out the way to use the updated

--- Jinwei_Zhang/lps.js/test/lps.js/index.test.js
+++ Jinwei_Zhang/lps.js/test/lps.js/index.test.js
@@ -2,7 +2,7 @@
   This file is part of the lps.js project, released open source under
   the BSD 3-Clause license. For more info, please see https://github.com/mauris/lps.js
  */
-
+const lpsRequire = require('./lpsRequire');
 const LPS = lpsRequire('LPS');
 
 const chai = require('chai');
--- Jinwei_Zhang/lps.js/test/programs/index.test.js
+++ Jinwei_Zhang/lps.js/test/programs/index.test.js
@@ -2,7 +2,8 @@
   This file is part of the lps.js project, released open source under
   the BSD 3-Clause license. For more info, please see https://github.com/mauris/lps.js
  */
-
+// global.lpsRequire = require('../src/lpsRequire');
+const lpsRequire = require('../../src/lpsRequire');
 const LPS = lpsRequire('LPS');
 const Tester = lpsRequire('engine/tester/Tester');
 
