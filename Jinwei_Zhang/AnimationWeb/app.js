6a1186f231062c8dd6e2cd58cb892d9ccabd104f
jinwei14 <janviezhang14@gmail.com>
2019-05-28 13:38:48 +0100

    more ideads

--- Jinwei_Zhang/AnimationWeb/app.js
+++ Jinwei_Zhang/AnimationWeb/app.js
@@ -1,7 +1,7 @@
 PIXI.utils.sayHello();
 
 
-const app = new PIXI.Application({ backgroundColor: 0x1099bb, width: 1200, height: 800 });
+const app = new PIXI.Application({ backgroundColor: 0x1099bb, width: 1200, height: 900 });
 document.body.appendChild(app.view);
 
 // const basicText = new PIXI.Text('Basic text in pixi');
@@ -33,19 +33,40 @@ richText.y = 20;
 
 app.stage.addChild(richText);
 
+const graphics = new PIXI.Graphics();
+ //main street
+graphics.lineStyle(2, 0xFFFFFF, 1);
+graphics.beginFill(0x333);
+graphics.drawRect(200, 500, 900, 70);
+graphics.endFill();
 
 // create a new Sprite from an image path
 const myCar = PIXI.Sprite.from('imgs/myCar.png');
+const yourCar = PIXI.Sprite.from('imgs/yourCar.png');
+const troubleMaker = PIXI.Sprite.from('imgs/troubleMaker.png');
+const otherCar = PIXI.Sprite.from('imgs/otherCar.png');
+const broken = PIXI.Sprite.from('imgs/broken.png');
 
 // center the sprite's anchor point
-myCar.x = 500;
-myCar.y = 500;
+myCar.x = 200;
+myCar.y = 100;
 
+yourCar.x = 600;
+yourCar.y = 100;
+
+troubleMaker.x = 600;
+troubleMaker.y = 200;
+
+otherCar.x = 1000;
+otherCar.y = 500;
+
+broken.x = 200;
+broken.y = 700;
 // move the sprite to the center of the screen
 // myCar.x = app.screen.width / 20;
 // myCar.y = app.screen.height / 20;
 
-app.stage.addChild(myCar);
+app.stage.addChild(graphics,myCar,yourCar,troubleMaker,otherCar,broken);
 
 // Listen for animate update
 app.ticker.add((delta) => {
@@ -53,6 +74,7 @@ app.ticker.add((delta) => {
     // delta is 1 if running at 100% performance
     // creates frame-independent transformation
     // bunny.rotation += 0.1 * delta;
-    myCar.x += 2;
-    myCar.y += 2;
+    if (otherCar.x >= 200){
+        otherCar.x -= 2;
+    }
 });
