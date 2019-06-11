0ab1044b644d7d17f64502e5ac5e2d0928f91415
jinwei14 <janviezhang14@gmail.com>
2019-06-11 12:00:38 +0100

    finish the parser class

--- Jinwei_Zhang/NodejsLearning/testParser2.js
+++ Jinwei_Zhang/NodejsLearning/testParser2.js
@@ -1,5 +1,6 @@
 const INDENTATION = '  ';
 // this the object that need to be process at every time cycle
+// loc(car, 1650, 340)).
 // location(yourCar, coordinate(9, 9), eastward)
 function ResultDict(fullPhrase, timeStamp) {
 
@@ -18,12 +19,18 @@ function ResultDict(fullPhrase, timeStamp) {
 
     //the object that is changing such as Car , Truck etx
     this.getObject = function () {
-        var startPos = fullPhrase.indexOf('(');
-        var endPos = fullPhrase.indexOf(',');
-        return fullPhrase.slice(startPos + 1, endPos);
+        var coordinate = ['coordinate', 'loc', 'location', 'coor','pos','position','xy'];
+        var len = this.matchArray.length;
+        //if the word is not one of the words in the coordinate array and not a digit it will be the name
+        //of the object
+        for (var i = 1; i < len; i++) {
+            if (isNaN(this.matchArray[i] ) && coordinate.includes(this.matchArray[i].toLowerCase()) === false){
+                return this.matchArray[i];
+            }
+        }
     };
 
-    // should the fluent that changed.
+    // should be the fluent that changed such as : loc, location.
     this.getFluent = function () {
         var endPos = fullPhrase.indexOf('(');
         return fullPhrase.slice(0, endPos);
@@ -41,18 +48,18 @@ function ResultDict(fullPhrase, timeStamp) {
         return retList;
     };
 
-    //the heading is optional.
+    //the heading is optional. If there is a heading then get the heading as the form of
     this.getHeading = function () {
-        var orintation =
+        var orientation =
             [
                 'north', 'south', 'west', 'east',
                 'up', 'down', 'left', 'right',
                 'northward', 'southward', 'westward', 'eastward'
             ];
-
         var len = this.matchArray.length;
         for (var i = 0; i < len; i++) {
-            if (orintation.includes(this.matchArray[i])){
+            //check if any of the works in the matching array is in the orientation array
+            if (orientation.includes(this.matchArray[i].toLowerCase())){
                 return this.matchArray[i];
             }
         }
@@ -63,10 +70,10 @@ function ResultDict(fullPhrase, timeStamp) {
 //location(yourCar, coordinate(9, 9), eastward)
 //loc(car, 1650, 340))
 //position(dummyCar, xy(66.90)).
-var obj2 = new ResultDict('location(yourCar, coordinate(9, 9), eastward)', 30);
+var obj2 = new ResultDict('position(dummyCar, xy(66.90)).', 30);
 console.log('Fluent: ' + obj2.getFluent());
 console.log('heading: ' + obj2.getHeading());
-console.log('Object:' + obj2.getObject());
+console.log('Object: ' + obj2.getObject());
 console.log('Position: ' + obj2.getPosition());
 console.log('Timestamp: ' + obj2.timeStamp);
 
