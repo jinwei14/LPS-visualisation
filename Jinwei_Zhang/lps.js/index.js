eaab15fccd63376870980b8b03aa09d66f627042
jinwei14 <janviezhang14@gmail.com>
2019-06-16 13:07:55 +0100

    adding more lps code

--- Jinwei_Zhang/lps.js/index.js
+++ Jinwei_Zhang/lps.js/index.js
@@ -171,14 +171,14 @@ function generateSpec(programFile, specFile) {
         console.log(err);
       });
 
-      if (specFile !== null) {
-        // write to file when program is done
-        engine.on('done', () => {
-          // fs.writeFile(specFile, buffer, () => {
-          //   Logger.log('Spec file written to ' + specFile);
-          // });
-        });
-      }
+      // if (specFile !== null) {
+      //   // write to file when program is done
+      //   engine.on('done', () => {
+      //     // fs.writeFile(specFile, buffer, () => {
+      //     //   Logger.log('Spec file written to ' + specFile);
+      //     // });
+      //   });
+      // }
 
       // Logger.log('Executing ' + programFile);
       // Logger.log('-----');
@@ -220,6 +220,7 @@ function loadExample(element) {
   // request.send();
 }
 
+// eslint-disable-next-line no-unused-vars
 function runProgram(program) {
   // var source = txtCodeInput.value;
   // outputArea.innerHTML = '';
@@ -232,7 +233,7 @@ function runProgram(program) {
 
       // let executionProgressBar = document.getElementById('executionProgressBar');
 
-      engine.setContinuousExecution(true);
+      // engine.setContinuousExecution(true);
 
       engine.on('error', (err) => {
         // outputArea.innerHTML = '<p>Error: ' + err + '</p>';
@@ -250,70 +251,70 @@ function runProgram(program) {
         console.log(engine.getActiveFluents());
       });
 
-      engine.on('done', () => {
-        let maxCycles = result.length;
-        for (let i = 0; i < maxCycles; i += 1) {
-          result[i].fluents = result[i].fluents
-            .map((f) => {
-              return {
-                term: f,
-                length: 1
-              };
-            });
-          result[i].overlappingFluents = 0;
-        }
-
-        for (let i = 0; i < maxCycles; i += 1) {
-          let newFluents = [];
-          let lastSeenCycle = i;
-          let numInCycle = {};
-
-          for (let j = i + 1; j < maxCycles; j += 1) {
-            numInCycle[j] = 0;
-          }
-          result[i].fluents
-            .forEach((fArg) => {
-              let f = fArg;
-              for (let j = i + 1; j < maxCycles; j += 1) {
-                let hasSameFluent = false;
-                result[j].fluents = result[j].fluents
-                  .filter((otherF) => {
-                    if (otherF.term === f.term) {
-                      numInCycle[j] += 1;
-                      f.length += 1;
-                      hasSameFluent = true;
-                      return false;
-                    }
-                    return true;
-                  });
-                if (!hasSameFluent) {
-                  break;
-                }
-                if (j > lastSeenCycle) {
-                  lastSeenCycle = j;
-                }
-              }
-              for (let j = i + 1; j <= lastSeenCycle; j += 1) {
-                result[j].overlappingFluents = result[i].overlappingFluents
-                  + numInCycle[j];
-              }
-              newFluents.push(f);
-            });
-          // ensure that fluents are sorted by the longest ones first
-          newFluents.sort((a, b) => {
-            if (a.length === b.length) {
-              return 0;
-            }
-            return a.length > b.length ? -1 : 1;
-          });
-          result[i].fluents = newFluents;
-        }
-        console.log(result);
-        console.log(result[0].fluents);
-        // btnRunProgram.disabled = false;
-        // let table = buildTableForResults(result);
-        // outputArea.appendChild(table);
-      });
+      // engine.on('done', () => {
+      //   let maxCycles = result.length;
+      //   for (let i = 0; i < maxCycles; i += 1) {
+      //     result[i].fluents = result[i].fluents
+      //       .map((f) => {
+      //         return {
+      //           term: f,
+      //           length: 1
+      //         };
+      //       });
+      //     result[i].overlappingFluents = 0;
+      //   }
+      //
+      //   for (let i = 0; i < maxCycles; i += 1) {
+      //     let newFluents = [];
+      //     let lastSeenCycle = i;
+      //     let numInCycle = {};
+      //
+      //     for (let j = i + 1; j < maxCycles; j += 1) {
+      //       numInCycle[j] = 0;
+      //     }
+      //     result[i].fluents
+      //       .forEach((fArg) => {
+      //         let f = fArg;
+      //         for (let j = i + 1; j < maxCycles; j += 1) {
+      //           let hasSameFluent = false;
+      //           result[j].fluents = result[j].fluents
+      //             .filter((otherF) => {
+      //               if (otherF.term === f.term) {
+      //                 numInCycle[j] += 1;
+      //                 f.length += 1;
+      //                 hasSameFluent = true;
+      //                 return false;
+      //               }
+      //               return true;
+      //             });
+      //           if (!hasSameFluent) {
+      //             break;
+      //           }
+      //           if (j > lastSeenCycle) {
+      //             lastSeenCycle = j;
+      //           }
+      //         }
+      //         for (let j = i + 1; j <= lastSeenCycle; j += 1) {
+      //           result[j].overlappingFluents = result[i].overlappingFluents
+      //             + numInCycle[j];
+      //         }
+      //         newFluents.push(f);
+      //       });
+      //     // ensure that fluents are sorted by the longest ones first
+      //     newFluents.sort((a, b) => {
+      //       if (a.length === b.length) {
+      //         return 0;
+      //       }
+      //       return a.length > b.length ? -1 : 1;
+      //     });
+      //     result[i].fluents = newFluents;
+      //   }
+      //   console.log(result);
+      //   console.log(result[0].fluents);
+      //   // btnRunProgram.disabled = false;
+      //   // let table = buildTableForResults(result);
+      //   // outputArea.appendChild(table);
+      // });
 
       engine.run();
     })
