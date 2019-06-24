3f79201e5bf0dbcff0cef5f4af850b4e705a845c
jinwei14 <janviezhang14@gmail.com>
2019-06-24 14:12:01 +0100

    traffic light position moving

--- Jinwei_Zhang/AnimationWeb/scripts/prepossessing.js
+++ Jinwei_Zhang/AnimationWeb/scripts/prepossessing.js
@@ -252,14 +252,14 @@
                         });
                         //Do something
                     } else if (currentTime > 1) {
-                        // console.log(currentTime);
-                        // fluents.forEach(function (item, index) {
-                        //     if (item.toLowerCase().startsWith('location')) {
-                        //         console.log(item);
-                        //         var loc = new VehicleLoc(item, currentTime);
-                        //         appManager.changeVehicleLocation(loc.getObjectName(), loc.X, loc.Y, loc.getHeading());
-                        //     }
-                        // });
+                        console.log(currentTime);
+                        fluents.forEach(function (item, index) {
+                            if (item.toLowerCase().startsWith('location')) {
+                                console.log(item);
+                                var loc = new VehicleLoc(item, currentTime);
+                                appManager.changeVehicleLocation(loc.getObjectName(), loc.X, loc.Y, loc.getHeading());
+                            }
+                        });
                     }
                 });
 
