3f5bee5ddf8c8ac52d2d070afabff73f95403e4c
jinwei14 <janviezhang14@gmail.com>
2019-07-25 11:03:37 +0100

    re-format code

--- Jinwei_Zhang/AnimationWebBackEnd/backEndExpress/routes/index.js
+++ Jinwei_Zhang/AnimationWebBackEnd/backEndExpress/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express on AWS' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express on AWS'});
 });
 
 module.exports = router;
