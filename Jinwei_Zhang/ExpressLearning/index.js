287df398f4fc5d37ad483444d6b0dfcb3029b7aa
jinwei14 <janviezhang14@gmail.com>
2019-07-27 21:06:47 +0100

    server side build

--- Jinwei_Zhang/ExpressLearning/index.js
+++ Jinwei_Zhang/ExpressLearning/index.js
@@ -1,33 +1,20 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-const members = [
-    {
-        id: 1,
-        name: 'John Doe',
-        email: 'john@gmail.com',
-        status: 'active'
-    },
-    {
-        id: 2,
-        name: 'Bob Williams',
-        email: 'bob@gmail.com',
-        status: 'inactive'
-    },
-    {
-        id: 3,
-        name: 'Shannon Jackson',
-        email: 'shannon@gmail.com',
-        status: 'active'
-    }
-];
+// const logger = require('./middleware/logger');
+// //init middleware
+// app.use(logger);
 
 
-app.get('/api/members',(req,res)=>{
-    res.json(members);
-});
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended : false}));
+
+app.use('/api/members',require('./routes/api/members'));
+
+
+
 // //this is not ideal we want to make publc folder static
 // app.get('/', (req,res)=>{
 //    //could send file/ json/ render html
@@ -40,7 +27,7 @@ app.use(express.static(path.join(__dirname,'public')));
 //means: whatever is in the environment variable PORT, or 3000 if there's nothing there.
 //If you pass 3000 hard-coded to app.listen(), you're always listening on port 3000, which might be just for you, or not,
 // depending on your requirements and the requirements of the environment in which you're running your server.
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8081;
 
 // can not get/
 app.listen(PORT,()=>console.log(`server started on port ${PORT}`));
